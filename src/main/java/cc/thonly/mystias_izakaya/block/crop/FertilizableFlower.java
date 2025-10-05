7f88427b3fa77392b92e2b0fe3b5153d08c3663e
Nippaku_Zanmu <120476813+Nippaku-Zanmu@users.noreply.github.com>
2025-10-05 22:00:54 +0800

    幻昙花催长

---
+++ src/main/java/cc/thonly/mystias_izakaya/block/crop/FertilizableFlower.java
@@ -0,0 +1,34 @@
+package cc.thonly.mystias_izakaya.block.crop;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Fertilizable;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+
+public class FertilizableFlower extends FlowerBlock implements Fertilizable {
+    public FertilizableFlower(RegistryEntry<StatusEffect> stewEffect, float effectLengthInSeconds, Settings settings) {
+        super(stewEffect, effectLengthInSeconds, settings);
+    }
+
+    @Override
+    public boolean isFertilizable(WorldView world, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
+        dropStack(world, pos, new ItemStack(this));
+    }
+}
